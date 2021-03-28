0d721700f734fe39b3540549f5e999163410c0f1
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-03-28 23:01:12 +0500

    Added Header component

---
+++ pwa/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../assests/images/icon.png'
+
+const Header = () =>{
+    return (
+        <div id="Header" >
+        <img src={logo} alt="logo"/>
+            <h1>Chatastrophe</h1>
+        </div>
+    )
+}
+
+export default Header;
