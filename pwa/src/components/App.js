07b96d0a18a0e1cc8a6cb4bfbef082485bc06fa9
Malaika Afridi <57406809+malaika22@users.noreply.github.com>
2021-04-03 13:11:54 +0500

    Implemented routing for Chat Component

--- pwa/src/components/App.js
+++ pwa/src/components/App.js
@@ -1,12 +1,37 @@
+import React , {Component} from 'react'
+import {Route, withRouter} from 'react-router-dom'
 import './App.css';
 import LoginContainer from './LoginContainer/LoginContainer'
+import ChatContainer from './ChatContainer/ChatContainer'
+import firebase from '../fbConfig'
 
-function App() {
-  return (
-    <div id="container" className="inner-container">
-      <LoginContainer/>
-    </div>
-  );
+class App extends Component {
+  state= { 
+    user: null
+  }
+
+  componentDidMount(){
+    firebase.auth().onAuthStateChanged(user=>{
+      if(user) {
+        console.log(user)
+        this.setState({user})
+      }  else {
+        this.props.history.push("/login")
+      }
+    })
+  }
+  render()
+  {
+    return (
+      <div id="container" className="inner-container">
+        <Route path="/login" component={LoginContainer} />
+        <Route path="/" component={ChatContainer} />
+      </div>
+    )
+  
+  }
+
+
 }
 
-export default App;
+export default withRouter(App);
